0d4aab954c5b1e58a83298a5ac5066969f4120e7
Sam Moss <sam.moss321@gmail.com>
2021-08-12 12:17:51 -0700

    updates footer

--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,22 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Header.module.css";
+import styles from "../styles/Footer.module.css";
 import Link from "next/link";
-import IconLink from "./icon_link.js";
 
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <p>footer</p>
+      <Image
+        src={"/images/patreon-creators-patreon.png"}
+        width={130}
+        height={130}
+        className={styles.footerImage}
+      />
+      <div className={styles.text}>
+        In order to best support our collaborators,CLOAK is funded exclusively
+        through our Patreon, with 100 percent of royalties being paid to authors
+        & artists.
+      </div>
     </div>
   );
 }
