6823c89e41c71299c0d011cdd5e21c37edea6187
Sam Moss <sam.moss321@gmail.com>
2021-08-12 13:25:13 -0700

    styles home text

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,15 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <p>
-      CLOAK is an interdisciplinary publishing project focused on visibility,
-      concealment, and surveillance.
-      <br />
-      Each work we publish is designed to best fit the medium of its release.
-      Whether book, executable, or pdf.
-    </p>
+    <div className={styles.homeText}>
+      <p>
+        CLOAK is an interdisciplinary publishing project focused on visibility,
+        concealment, and surveillance.
+        <br />
+        <br />
+        Each work we publish is designed to best fit the medium of its release.
+        Whether book, executable, or pdf.
+      </p>
+    </div>
   );
 }
