8bf7806342cd37f356219347800e3e8345b9a790
Sam Moss <sam.moss321@gmail.com>
2021-08-14 13:25:28 -0700

    code for work entries

--- pages/catalogue.js
+++ pages/catalogue.js
@@ -1,7 +1,21 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Catalogue.module.css";
+import { works } from "../public/works.js";
 
 export default function Catalogue() {
-  return <p>Catalogue</p>;
+  function workEntries() {
+    let worksHtml = works.map((entry) => (
+      <div className={styles.workEntry} key={entry.title}>
+        <a target="_blank" rel="noopener noreferrer" href={entry.url}>
+          <img src={entry.imgUrl} className={styles.entryImage} />
+        </a>
+        <div className={styles.entryText}>{entry.text}</div>
+      </div>
+    ));
+
+    return worksHtml;
+  }
+
+  return <div className={styles.worksContainer}>{workEntries()}</div>;
 }
---
+++ public/works.js
@@ -0,0 +1,29 @@
+// {
+//   img: relative_image_link,
+//   title: "unique_title_for_work"
+//   url: www.work-location.com
+//   text: 'description of the work'
+// }
+
+export const works = [
+  {
+    imgUrl: "http://placekitten.com/250/250",
+    title: "happy_kitten",
+    url: "www.twitter.com",
+    text:
+      "Average Description - Rum laboria plaut eum re licid ut aut dernatio. Aligentiunt omnisquias et etumet et mo volutaCone consequ aspelitam dol- lore stiunt, tem estrum am in cus si coribeaquas ut voluptat",
+  },
+  {
+    imgUrl: "http://placekitten.com/250/250",
+    title: "grumpy_kitten",
+    url: "www.twitter.com",
+    text: "Short Description",
+  },
+  {
+    imgUrl: "http://placekitten.com/250/250",
+    title: "sassy_kitten",
+    url: "www.twitter.com",
+    text:
+      "Long Description - Birth rich in mystery prime number not a sunrise but a galaxyrise corpus callosum the only home we've ever known. Network of wormholes across the centuries as a patch of light from which we spring paroxysm of global death network of wormholes. A very small stage in a vast cosmic arena great turbulent clouds permanence of the stars with pretty stories for which there's little good evidence the only home we've ever known circumnavigated and billions upon billions upon billions upon billions upon billions upon billions upon billions.",
+  },
+];
