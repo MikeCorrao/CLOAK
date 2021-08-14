b7fb0d2a39e9e66af25994af3b2919ee46833ef7
Sam Moss <sam.moss321@gmail.com>
2021-08-14 12:10:17 -0700

    makes tab title cloak

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import "../styles/globals.css";
 import Layout from "../components/layout.js";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>cloak</title>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
