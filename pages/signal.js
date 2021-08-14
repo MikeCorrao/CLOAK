60c4c6b49a9ab001b967f1ca2680626aa2ab8def
Sam Moss <sam.moss321@gmail.com>
2021-08-14 11:18:29 -0700

    adds signal page text and styling;

--- pages/signal.js
+++ pages/signal.js
@@ -1,7 +1,36 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Signal.module.css";
 
 export default function Signal() {
-  return <p>SIgnal</p>;
+  return (
+    <div className={styles.signalText}>
+      <p>
+        The CLOAK is a means of obfuscation. Not by rendering itself
+        undetectable, but rather by creating a surface which conceals its
+        contents. Allowing for the user to detect action, but not to discern
+        what that particular action really is.
+      </p>
+
+      <p>A blurring flourish / stealth weapon / a vestigial skin</p>
+
+      <p>
+        The role of the object is two-fold. First to position itself as
+        something independent of the user (which does
+        notrelyonthesubject’scapacityto interface). And second, to observe /
+        document its environment—looking back as it is looked upon. Articulating
+        a multi-directionality within the acts of READING & VIEWING.
+      </p>
+
+      <p>
+        The CLOAK is an anti-solarist tool. Its creation is distinctly digital.
+        The text is written on a computer. In Word, Google Drive, Notepad. Its
+        shape is arranged on InDesign, PowerPoint, Rhino. Its body is produced
+        by IngramSpark, KDP, BookMobile.
+      </p>
+      <a href="">
+        To learn more, considering purchasing our introductory release.
+      </a>
+    </div>
+  );
 }
