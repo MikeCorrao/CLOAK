cd497260eaaecba9b17f03951e79de3fff9f3c7a
Sam Moss <sam.moss321@gmail.com>
2021-08-16 15:43:57 -0700

    centers footer text and some other work

--- components/footer.js
+++ components/footer.js
@@ -6,16 +6,24 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <Image
-        src={"/images/patreon-creators-patreon.png"}
-        width={130}
-        height={130}
-        className={styles.footerImage}
-      />
-      <div className={styles.text}>
-        In order to best support our collaborators,CLOAK is funded exclusively
-        through our Patreon, with 100 percent of royalties being paid to authors
-        & artists.
+      <div className={styles.footerLeft}>
+        <img
+          src={"/images/patreon-creators-patreon.png"}
+          width={100}
+          height={100}
+          className={styles.footerImage}
+        />
+      </div>
+      <div className={styles.footerCenterLeft}></div>
+      <div className={styles.footerCenterRight}>
+        <div className={styles.text}>
+          In order to best support our collaborators,CLOAK is funded exclusively
+          through our Patreon, with 100 percent of royalties being paid to
+          authors & artists.
+        </div>
+      </div>
+      <div className={styles.footerRight}>
+        <div></div>
       </div>
     </div>
   );
