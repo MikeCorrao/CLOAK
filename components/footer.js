1c8054bf10640dc440c39f71fc63c11e400684e1
Sam Moss <sam.moss321@gmail.com>
2021-08-18 12:04:15 -0700

    decrease patreon image size

--- components/footer.js
+++ components/footer.js
@@ -9,8 +9,8 @@ export default function Footer() {
       <div className={styles.footerLeft}>
         <img
           src={"/images/patreon-creators-patreon.png"}
-          width={100}
-          height={100}
+          width={80}
+          height={80}
           className={styles.footerImage}
         />
       </div>
