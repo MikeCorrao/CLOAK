1506546220e257b3a8820210016f7121bdf61283
Sam Moss <sam.moss321@gmail.com>
2021-09-12 09:21:59 -0700

    pulls in CLOAK3DLOGO obj and mtl, gets them orientied

--- components/objectViewer.js
+++ components/objectViewer.js
@@ -37,7 +37,7 @@ export default function ObjectViewer(props) {
       scene.add(light);
     }
 
-    const fov = 45;
+    const fov = 35;
     const aspect = 2;
     const near = 0.1;
     const far = 100;
@@ -46,48 +46,30 @@ export default function ObjectViewer(props) {
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.enabled = false;
 
-    camera.position.set(5, 2, 5);
+    camera.position.set(12, 10, 12);
     camera.lookAt(controls.target);
 
     controls.enabled = true;
-    controls.target.set(1.5, 2, 1.5);
+    controls.target.set(0, 9, 0);
     controls.update();
 
     {
       const mtlLoader = new MTLLoader();
-      mtlLoader.load("objs/Metal.rmtl", (materials) => {
+      mtlLoader.load("objs/CLOAK3DLOGO.mtl", (materials) => {
         materials.preload();
         const objLoader = new OBJLoader();
         objLoader.setMaterials(materials);
-        objLoader.load(`objs/${title}.obj`, (obj) => {
+        objLoader.load(`objs/CLOAK3DLOGO.obj`, (obj) => {
           scene.add(obj);
           obj.scale.multiplyScalar(0.3);
-          obj.position.set(3, -2, 0);
-          obj.rotation.x = Math.PI;
-          obj.rotation.y = -(Math.PI / 5);
+          obj.position.set(-2, 0, 0);
+          obj.rotation.z = -Math.PI / 7;
+          obj.rotation.x = Math.PI / 5;
+          obj.rotation.y = Math.PI / 8;
           scene.add(obj);
           renderer.render(scene, camera);
           requestAnimationFrame(render);
         });
-        // obj.traverse(
-        //   function (child) {
-        //     scene.add(obj);
-        //     obj.scale.multiplyScalar(0.3);
-        //     obj.position.set(3, -2, 0);
-        //     obj.rotation.x = Math.PI;
-        //     obj.rotation.y = -(Math.PI / 5);
-        //     scene.add(obj);
-        //     renderer.render(scene, camera);
-        //     requestAnimationFrame(render);
-        //   },
-        //   function (xhr) {
-        //     console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-        //   },
-        //   function (error) {
-        //     console.log("An error happened", error);
-        //   }
-        // );
-        // });
       });
     }
 
